615d2398fcf85db9aff8adbb33010e5cbbb85103
Ricardo <ricardomorim05@gmail.com>
2024-05-16 22:01:59 +0100

    started working on US15

--- src/main/java/pt/ipp/isep/dei/mdisc/Edge.java
+++ src/main/java/pt/ipp/isep/dei/mdisc/Edge.java
@@ -1,18 +1,13 @@
 package pt.ipp.isep.dei.mdisc;
 
 
-public class Edge implements Comparable<Edge> {
+public class Edge {
     int src, dest, weight;
 
     public Edge(int src, int dest, int weight) {
         this.src = src;
         this.dest = dest;
         this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Edge other) {
-        return this.weight - other.weight;
     }
 }
 
