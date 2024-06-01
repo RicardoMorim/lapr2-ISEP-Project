3421a4c17291798ce658f7eb7701edae74fb803d
Goncalo <1231170@isep.ipp.pt>
2024-06-01 23:17:07 +0100

    Updated the project-domain-model

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Urgency.java
@@ -0,0 +1,16 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+public enum Urgency {
+    /**
+     * Low urgency.
+     */
+    LOW,
+    /**
+     * Medium urgency.
+     */
+    MEDIUM,
+    /**
+     * High urgency.
+     */
+    HIGH
+}
