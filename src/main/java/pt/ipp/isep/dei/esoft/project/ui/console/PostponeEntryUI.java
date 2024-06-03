fb5a5ea97b56d70aad3b9e69e939efd2016c6e08
Goncalo <1231170@isep.ipp.pt>
2024-06-03 21:29:39 +0100

    Created the tests needed dor US20 and 26

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/PostponeEntryUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/PostponeEntryUI.java
@@ -2,6 +2,7 @@ package pt.ipp.isep.dei.esoft.project.ui.console;
 
 import pt.ipp.isep.dei.esoft.project.application.controller.AgendaController;
 import pt.ipp.isep.dei.esoft.project.domain.AgendaEntry;
+import pt.ipp.isep.dei.esoft.project.domain.Status;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
 import java.util.Date;
@@ -22,11 +23,9 @@ public class PostponeEntryUI implements Runnable {
 
         AgendaEntry chosenEntry = agendaEntries.get(entryIndex);
 
-        Date newDate = Utils.readDateFromConsole("Enter the date in the format dd-MM-yyyy: ");
+        chosenEntry = agendaController.postponeEntry(chosenEntry);
 
-        chosenEntry = agendaController.postponeEntry(chosenEntry, newDate);
-
-        if (chosenEntry.getDate().equals(newDate))
+        if (chosenEntry.getStatus().equals(Status.POSTPONED))
             System.out.println("Entry postponed successfully");
         else
             System.out.println("Unable to postpone entry");
